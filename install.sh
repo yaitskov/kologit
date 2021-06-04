a2adfb5489fade033c6cabdf97c8e7ed9ef9471d
Daniil Iaitskov <daniil.iaitskov@soostone.com>
2021-06-04 12:50:08 -0400

    master installer

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+
+
+mkdir -p ~/.bin/scripts
+pushd ~/.bin/scripts
+git clone https://github.com/yaitskov/kologit.git
+popd
+
+cat <<EOF >> ~/.bashrc
+export PATH=\$HOME/.bin/scripts/kologit:\$PATH
+EOF
+
+
+cat<<EOF
+You can update global git config to ignore kologit system folder:
+ ~/.gitconfig:
+[core]
+	pager = cat
+	excludesfile = /home/bob/.gitignore
+
+~/.gitignore:
+.kologit
+EOF
